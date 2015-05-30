9415c3142bfd228720b2925c012b79d4fc8f14ca
Sebastian Mellor <www-data@sbl.io>
2015-05-30 23:42:26 +0100

    mime types

--- server/list.php
+++ server/list.php
@@ -1,3 +1,3 @@
 <?php
 $user = preg_replace('[^a-z]','',$_GET['user']);
-echo `ls uploads/$user-*.wav`;
+echo `ls uploads/$user-*`;
--- server/rcv.php
+++ server/rcv.php
@@ -5,8 +5,21 @@ foreach(array('video', 'audio') as $type) {
         $fileName = $_POST["${type}-filename"];
         $uploadDirectory = 'uploads/'.$fileName;
 
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_file($finfo, $_FILES["${type}-blob"]['tmp_name']);
+        finfo_close($finfo);
+        echo $mime."\n";
+
+        $ext = 'audio';
+        if ($mime == 'audio/x-wav') {
+            $ext = 'wav';
+        }
+        if ($mime == 'application/ogg') {
+            $ext = 'ogg';
+        }
+
         $r = rand(10000,99999);
-        $f = 'uploads/sam-'.$r.'.wav';
+        $f = 'uploads/sam-'.$r.'.'.$ext;
 
         if (!move_uploaded_file($_FILES["${type}-blob"]["tmp_name"], $f)) {
             echo(" problem moving uploaded file");
